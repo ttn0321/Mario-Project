a217bc8c72ea9c759ec03e24c18a21f7f29b69b6
ttn0321 <thanhnhanta03@gmail.com>
2024-05-20 19:00:03 +0700

    Koopa Timeout and Transition

--- Mario Project/Koopa.cpp
+++ Mario Project/Koopa.cpp
@@ -1,124 +1,136 @@
 #include "Koopa.h"
 
-
 CKoopa::CKoopa(float x, float y) :CGameObject(x, y)
 {
-	this->ax = 0;
-	this->ay = KOOPA_GRAVITY;
-	die_start = -1;
-	SetState(KOOPA_STATE_WALKING);
+    this->ax = 0;
+    this->ay = KOOPA_GRAVITY;
+    state_start = -1;
+    SetState(KOOPA_STATE_WALKING);
 }
 
 void CKoopa::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
-	if (state == KOOPA_STATE_SHELL || state == KOOPA_STATE_SHELL_SLIDE)
-	{
-		left = x - KOOPA_BBOX_WIDTH / 2;
-		top = y - KOOPA_BBOX_HEIGHT_DIE / 2;
-		right = left + KOOPA_BBOX_WIDTH;
-		bottom = top + KOOPA_BBOX_HEIGHT_DIE;
-	}
-	else
-	{
-		left = x - KOOPA_BBOX_WIDTH / 2;
-		top = y - KOOPA_BBOX_HEIGHT / 2;
-		right = left + KOOPA_BBOX_WIDTH;
-		bottom = top + KOOPA_BBOX_HEIGHT;
-	}
+    if (state == KOOPA_STATE_SHELL || state == KOOPA_STATE_SHELL_SLIDE || state == KOOPA_STATE_TRANSITION)
+    {
+        left = x - KOOPA_BBOX_WIDTH / 2;
+        top = y - KOOPA_BBOX_HEIGHT_DIE / 2;
+        right = left + KOOPA_BBOX_WIDTH;
+        bottom = top + KOOPA_BBOX_HEIGHT_DIE;
+    }
+    else
+    {
+        left = x - KOOPA_BBOX_WIDTH / 2;
+        top = y - KOOPA_BBOX_HEIGHT / 2;
+        right = left + KOOPA_BBOX_WIDTH;
+        bottom = top + KOOPA_BBOX_HEIGHT;
+    }
 }
 
 void CKoopa::OnNoCollision(DWORD dt)
 {
-	x += vx * dt;
-	y += vy * dt;
+    x += vx * dt;
+    y += vy * dt;
 };
 
 void CKoopa::OnCollisionWith(LPCOLLISIONEVENT e)
 {
-	if (!e->obj->IsBlocking()) return;
-	if (dynamic_cast<CKoopa*>(e->obj)) return;
+    if (!e->obj->IsBlocking()) return;
+    if (dynamic_cast<CKoopa*>(e->obj)) return;
 
-	if (e->ny != 0)
-	{
-		vy = 0;
-	}
-	else if (e->nx != 0)
-	{
-		vx = -vx;
-	}
+    if (e->ny != 0)
+    {
+        vy = 0;
+    }
+    else if (e->nx != 0)
+    {
+        vx = -vx;
+    }
 }
 
 void CKoopa::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	vy += ay * dt;
-	vx += ax * dt;
+    vy += ay * dt;
+    vx += ax * dt;
 
-	if ((state == KOOPA_STATE_SHELL) && (GetTickCount64() - die_start > KOOPA_DIE_TIMEOUT))
-	{
-		return;
-	}
-	if (state == KOOPA_STATE_WALKING && IsNearEdgeOfPlatform(coObjects))
-	{
-		vx = -vx; // Turn around
-	}
-	CGameObject::Update(dt, coObjects);
-	CCollision::GetInstance()->Process(this, dt, coObjects);
+    ULONGLONG now = GetTickCount64();
+
+    if ((state == KOOPA_STATE_SHELL || state == KOOPA_STATE_SHELL_SLIDE) && now - state_start > KOOPA_SHELL_TIMEOUT)
+    {
+        SetState(KOOPA_STATE_TRANSITION);
+    }
+    else if (state == KOOPA_STATE_TRANSITION && now - state_start > KOOPA_TRANSITION_TIMEOUT)
+    {
+        SetState(KOOPA_STATE_WALKING);
+        y -= (KOOPA_BBOX_HEIGHT - KOOPA_BBOX_HEIGHT_DIE) / 2;
+    }
+
+    if (state == KOOPA_STATE_WALKING && IsNearEdgeOfPlatform(coObjects))
+    {
+        vx = -vx; // Turn around
+    }
+
+    CGameObject::Update(dt, coObjects);
+    CCollision::GetInstance()->Process(this, dt, coObjects);
 }
-
 
 void CKoopa::Render()
 {
-	
-	int aniId= ID_ANI_KOOPA_WALKING_LEFT;
-	if (state == KOOPA_STATE_WALKING && vx < 0)
-		aniId = ID_ANI_KOOPA_WALKING_LEFT;
-	else if (state == KOOPA_STATE_WALKING && vx > 0)
-		aniId = ID_ANI_KOOPA_WALKING_RIGHT;
-	if (state == KOOPA_STATE_SHELL || state == KOOPA_STATE_SHELL_SLIDE)
-	{
-		aniId = ID_ANI_KOOPA_SHELL;
-	}
-
-	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
-	RenderBoundingBox();
+    int aniId = ID_ANI_KOOPA_WALKING_LEFT;
+    if (state == KOOPA_STATE_WALKING && vx < 0)
+        aniId = ID_ANI_KOOPA_WALKING_LEFT;
+    else if (state == KOOPA_STATE_WALKING && vx > 0)
+        aniId = ID_ANI_KOOPA_WALKING_RIGHT;
+    if (state == KOOPA_STATE_SHELL || state == KOOPA_STATE_SHELL_SLIDE)
+    {
+        aniId = ID_ANI_KOOPA_SHELL;
+    }
+    else if (state == KOOPA_STATE_TRANSITION)
+    {
+        aniId = ID_ANI_KOOPA_TRANSITION;
+    }
+    CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+    RenderBoundingBox();
 }
 
 void CKoopa::SetState(int state)
 {
-	CGameObject::SetState(state);
-	switch (state)
-	{
-	case KOOPA_STATE_SHELL:
-		die_start = GetTickCount64();
-		y += (KOOPA_BBOX_HEIGHT - KOOPA_BBOX_HEIGHT_DIE) / 2;
-		vx = 0;
-		vy = 0;
-		break;
-	case KOOPA_STATE_WALKING:
-		vx = -KOOPA_WALKING_SPEED;
-		break;
-	case KOOPA_STATE_SHELL_SLIDE:
-		vx = -KOOPA_SLIDING_SPEED;
-		break;
-	}
+    CGameObject::SetState(state);
+    state_start = GetTickCount64();
+    switch (state)
+    {
+    case KOOPA_STATE_SHELL:
+        y += (KOOPA_BBOX_HEIGHT - KOOPA_BBOX_HEIGHT_DIE) / 2;
+        vx = 0;
+        vy = 0;
+        break;
+    case KOOPA_STATE_SHELL_SLIDE:
+        vx = -KOOPA_SLIDING_SPEED;
+        break;
+    case KOOPA_STATE_WALKING:
+        vx = -KOOPA_WALKING_SPEED;
+        break;
+    case KOOPA_STATE_TRANSITION:
+        vx = 0;
+        break;
+    }
 }
 
 bool CKoopa::IsNearEdgeOfPlatform(vector<LPGAMEOBJECT>* coObjects)
 {
-	float xProbe = x + (vx > 0 ? KOOPA_BBOX_WIDTH / 2 : -KOOPA_BBOX_WIDTH / 2);
-	float yProbe = y + KOOPA_BBOX_HEIGHT / 2 + 1; // Slightly below the Koopa's feet
+    float xProbe = x + (vx > 0 ? KOOPA_BBOX_WIDTH / 2 : -KOOPA_BBOX_WIDTH / 2);
+    float yProbe = y + KOOPA_BBOX_HEIGHT / 2 + 1; // Slightly below the Koopa's feet
 
-	for (auto obj : *coObjects)
-	{
-		float l, t, r, b;
-		obj->GetBoundingBox(l, t, r, b);
-		if (dynamic_cast<CPlatform*>(obj)) // Assuming you have a platform class
-		{
-			if (xProbe > l && xProbe < r && yProbe > t && yProbe < b)
-			{
-				return false; // Ground detected
-			}
-		}
-	}
-	return true; // No ground detected
+    for (auto obj : *coObjects)
+    {
+        float l, t, r, b;
+        obj->GetBoundingBox(l, t, r, b);
+        if (dynamic_cast<CPlatform*>(obj)) // Assuming you have a platform class
+        {
+            if (xProbe > l && xProbe < r && yProbe > t && yProbe < b)
+            {
+                return false; // Ground detected
+            }
+        }
+    }
+    return true; // No ground detected
 }
--- Mario Project/Koopa.h
+++ Mario Project/Koopa.h
@@ -6,41 +6,43 @@
 #define KOOPA_WALKING_SPEED 0.05f
 #define KOOPA_SLIDING_SPEED 0.15f
 
-
 #define KOOPA_BBOX_WIDTH 16
 #define KOOPA_BBOX_HEIGHT 26
 #define KOOPA_BBOX_HEIGHT_DIE 16
 
-#define KOOPA_DIE_TIMEOUT 500
+#define KOOPA_SHELL_TIMEOUT 5000 // 5 seconds in shell state
+#define KOOPA_TRANSITION_TIMEOUT 2000 // 2 seconds in transition state
 
 #define KOOPA_STATE_WALKING 100
 #define KOOPA_STATE_SHELL 200
 #define KOOPA_STATE_SHELL_SLIDE 300
+#define KOOPA_STATE_TRANSITION 400
 
 #define ID_ANI_KOOPA_WALKING_LEFT 8000
 #define ID_ANI_KOOPA_WALKING_RIGHT 8001
 #define ID_ANI_KOOPA_SHELL 8002
+#define ID_ANI_KOOPA_TRANSITION 8003
 
 class CKoopa : public CGameObject
 {
 protected:
-	float ax;
-	float ay;
+    float ax;
+    float ay;
 
-	ULONGLONG die_start;
+    ULONGLONG state_start;
 
-	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
-	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
-	virtual void Render();
+    virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+    virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+    virtual void Render();
 
-	virtual int IsCollidable() { return 1; };
-	virtual int IsBlocking() { return 0; }
-	virtual void OnNoCollision(DWORD dt);
+    virtual int IsCollidable() { return 1; };
+    virtual int IsBlocking() { return 0; }
+    virtual void OnNoCollision(DWORD dt);
 
-	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
-	bool IsNearEdgeOfPlatform(vector<LPGAMEOBJECT>* coObjects);
+    virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+    bool IsNearEdgeOfPlatform(vector<LPGAMEOBJECT>* coObjects);
 
 public:
-	CKoopa(float x, float y);
-	virtual void SetState(int state);
+    CKoopa(float x, float y);
+    virtual void SetState(int state);
 };
