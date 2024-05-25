5260c8497e84b49166f53a00f5ee557c6143d45a
ttn0321 <thanhnhanta03@gmail.com>
2024-05-25 19:15:34 +0700

    Fire Plant Base Adjust

--- Mario Project/FirePlant.cpp
+++ Mario Project/FirePlant.cpp
@@ -1,15 +1,47 @@
 #include "FirePlant.h"
+CFirePlant::CFirePlant(float x, float y) :CGameObject(x, y)
+{
+	
+}
+
+void CFirePlant::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+	left = x - FIRE_PLANT_BBOX_WIDTH / 2;
+	top = y - FIRE_PLANT_BBOX_HEIGHT / 2;
+	right = left + FIRE_PLANT_BBOX_WIDTH;
+	bottom = top + FIRE_PLANT_BBOX_HEIGHT;
+	
+}
+
+void CFirePlant::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+};
+
+void CFirePlant::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+
+}
+
+void CFirePlant::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+
 void CFirePlant::Render()
 {
-	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(ID_ANI_FIRE_PLANT)->Render(x, y);
+	int aniId = ID_ANI_FIRE_PLANT;
+
+	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
 	RenderBoundingBox();
 }
 
-void CFirePlant::GetBoundingBox(float& l, float& t, float& r, float& b)
+void CFirePlant::SetState(int state)
 {
-	l = x - FIRE_PLANT_BBOX_WIDTH / 2;
-	t = y - FIRE_PLANT_BBOX_HEIGHT / 2;
-	r = l + FIRE_PLANT_BBOX_WIDTH;
-	b = t + FIRE_PLANT_BBOX_HEIGHT;
+	CGameObject::SetState(state);
+	
 }
--- Mario Project/FirePlant.h
+++ Mario Project/FirePlant.h
@@ -9,10 +9,26 @@
 #define FIRE_PLANT_BBOX_WIDTH 16
 #define FIRE_PLANT_BBOX_HEIGHT 32
 
-class CFirePlant : public CGameObject {
+class CFirePlant : public CGameObject
+{
+protected:
+	float ax;
+	float ay;
+
+	ULONGLONG die_start;
+
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void Render();
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+	virtual void OnNoCollision(DWORD dt);
+
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+	bool IsNearEdgeOfPlatform(vector<LPGAMEOBJECT>* coObjects);
+
 public:
-	CFirePlant(float x, float y) : CGameObject(x, y) {}
-	void Render();
-	void Update(DWORD dt) {}
-	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	CFirePlant(float x, float y);
+	virtual void SetState(int state);
 };
